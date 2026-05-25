1655d885c21d64af86f41aa0af7fbb231f82ab5e
Mutudu <aeondxf@live.com>
2026-05-25 15:29:43 +0800

    cleanup: remove redundant file-level usings covered by global usings

--- PicoJson/tests/PicoJson.Tests/JsonConverterTests.cs
+++ PicoJson/tests/PicoJson.Tests/JsonConverterTests.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace PicoJson.Tests;
 
 public class CustomDateConverter : IJsonConverter<DateTime>
--- PicoJson/tests/PicoJson.Tests/JsonSerializerTests.cs
+++ PicoJson/tests/PicoJson.Tests/JsonSerializerTests.cs
@@ -1,5 +1,3 @@
-using System.Buffers;
-
 namespace PicoJson.Tests;
 
 public class JsonSerializerTests
