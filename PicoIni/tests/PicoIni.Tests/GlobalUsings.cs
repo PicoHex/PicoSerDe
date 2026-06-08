a1480c3b10c02dd044e0e9732260643b80e6db15
Mutudu <aeondxf@live.com>
2026-06-08 11:34:51 +0800

    cleanup: remove duplicate global using in PicoIni.Tests/GlobalUsings.cs

--- PicoIni/tests/PicoIni.Tests/GlobalUsings.cs
+++ PicoIni/tests/PicoIni.Tests/GlobalUsings.cs
@@ -1,7 +1,6 @@
 global using System.Buffers;
 global using System.Text;
 global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.Configuration;
 global using PicoCrossValidation;
 global using PicoIni;
 global using PicoSerDe.Core;
