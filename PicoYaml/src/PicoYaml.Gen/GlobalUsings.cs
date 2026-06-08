61a47a078f0dd4b6fac3cbb91a9b06b21e73dcae
Mutudu <aeondxf@live.com>
2026-06-08 12:04:30 +0800

    cleanup: remove redundant global using Microsoft.CodeAnalysis.CSharp from Gen projects
    Microsoft.CodeAnalysis.CSharp.Syntax is kept (needed by shared GenInfrastructure.cs
    for InvocationExpressionSyntax, MemberAccessExpressionSyntax, etc.), but the parent
    namespace Microsoft.CodeAnalysis.CSharp is not used by any Gen project.

--- PicoYaml/src/PicoYaml.Gen/GlobalUsings.cs
+++ PicoYaml/src/PicoYaml.Gen/GlobalUsings.cs
@@ -4,6 +4,5 @@ global using System.Collections.Immutable;
 global using System.Linq;
 global using System.Text;
 global using Microsoft.CodeAnalysis;
-global using Microsoft.CodeAnalysis.CSharp;
-global using Microsoft.CodeAnalysis.CSharp.Syntax;
 global using Microsoft.CodeAnalysis.Text;
+global using Microsoft.CodeAnalysis.CSharp.Syntax;
